dc0e00b8071642b45333663ade9659775d3ed978
Anders Pearson <anders@columbia.edu>
2015-07-18 20:33:11 +0200

    let's not make that an exported type

--- clog.go
+++ clog.go
@@ -1,6 +1,6 @@
 package main
 
-type CircularLog struct {
+type circularLog struct {
 	fails     []int64
 	successes []int64
 	size      int64
@@ -16,12 +16,12 @@ type CircularLog struct {
 	percentChan chan *percentOp
 }
 
-// NewCircularLog constructs a new CircularLog and initializes it
-func NewCircularLog(size int64) *CircularLog {
+// NewCircularLog constructs a new circularLog and initializes it
+func NewCircularLog(size int64) *circularLog {
 	// go always initializes to zero value
 	fails := make([]int64, size)
 	successes := make([]int64, size)
-	c := CircularLog{
+	c := circularLog{
 		fails:     fails,
 		successes: successes,
 		size:      size,
@@ -59,7 +59,7 @@ type readResp struct {
 type totalOp struct{ Resp chan readResp }
 type percentOp struct{ Resp chan readResp }
 
-func (c *CircularLog) run() {
+func (c *circularLog) run() {
 	for {
 		select {
 		// writes
@@ -83,23 +83,23 @@ func (c *CircularLog) run() {
 	}
 }
 
-func (c *CircularLog) sumFails() int64 {
+func (c *circularLog) sumFails() int64 {
 	return sum(c.fails)
 }
 
-func (c *CircularLog) sumSuccesses() int64 {
+func (c *circularLog) sumSuccesses() int64 {
 	return sum(c.successes)
 }
 
 // Total returns the total number of requests logged
 // successes and failures
-func (c *CircularLog) Total() int64 {
+func (c *circularLog) Total() int64 {
 	r := make(chan readResp)
 	c.totalChan <- &totalOp{r}
 	return (<-r).T
 }
 
-func (c *CircularLog) total() int64 {
+func (c *circularLog) total() int64 {
 	return c.sumFails() + c.sumSuccesses()
 }
 
@@ -107,13 +107,13 @@ func (c *CircularLog) total() int64 {
 // essentially fails/total.
 // If there are no entries at all, it return 0.0 rather
 // than dividing by zero.
-func (c *CircularLog) Percent() float64 {
+func (c *circularLog) Percent() float64 {
 	r := make(chan readResp)
 	c.percentChan <- &percentOp{r}
 	return (<-r).P
 }
 
-func (c *CircularLog) percent() float64 {
+func (c *circularLog) percent() float64 {
 	t := c.sumFails() + c.sumSuccesses()
 	if t == 0 {
 		return 0.0
@@ -124,36 +124,36 @@ func (c *CircularLog) percent() float64 {
 // Advance moves to the next time segment
 // It advances the "write head" to the next cell (wrapping around)
 // and zeroes out the cell that it now points to.
-func (c *CircularLog) Advance() {
+func (c *circularLog) Advance() {
 	wait := make(chan waitResp)
 	c.advanceChan <- &advanceOp{wait}
 	<-wait
 }
 
-func (c *CircularLog) advance() {
+func (c *circularLog) advance() {
 	c.head = (c.head + 1) % c.size
 	c.successes[c.head] = 0
 	c.fails[c.head] = 0
 }
 
 // Success logs a success to the current cell
-func (c *CircularLog) Success() {
+func (c *circularLog) Success() {
 	wait := make(chan waitResp)
 	c.successChan <- &successOp{wait}
 	<-wait
 }
 
-func (c *CircularLog) success() {
+func (c *circularLog) success() {
 	c.successes[c.head] += 1
 }
 
 // Fail logs a failure to the current cell
-func (c *CircularLog) Fail() {
+func (c *circularLog) Fail() {
 	wait := make(chan waitResp)
 	c.failChan <- &failOp{wait}
 	<-wait
 }
 
-func (c *CircularLog) fail() {
+func (c *circularLog) fail() {
 	c.fails[c.head] += 1
 }
